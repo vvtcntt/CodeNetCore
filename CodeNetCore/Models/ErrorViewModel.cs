85524492b4862d64bbc07808f2018b2ef7a3cfd0
THIEPVU\ThiepVu <vvtcntt@gmail.com>
2018-02-22 11:28:48 +0700

    Add project files.

---
+++ CodeNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CodeNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
