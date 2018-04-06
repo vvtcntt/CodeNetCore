74f5139d0235634c1374c74fe56a3f332481ef40
Thiepvu <Thiepvu@THIEPVU>
2018-04-06 15:45:33 +0700

    lesson26 done

---
+++ CodeNetCore/Areas/Admin/Controlers/ProductCategoryController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodeNetCore.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeNetCore.Areas.Admin.Controlers
+{
+    public class ProductCategoryController : BaseController
+    {
+        IProductCategoryService _productCategoryService;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+ 
+        public ProductCategoryController( IProductCategoryService productCategoryService)
+        {
+            _productCategoryService = productCategoryService;
+        }
+       
+         [HttpGet]
+        public IActionResult GetAll()
+        {
+            var model = _productCategoryService.GetAll();
+            return new OkObjectResult(model);
+        }
+        
+        
+    }
+}
---
+++ CodeNetCore/wwwroot/app/controller/product-category/index.js
@@ -0,0 +1,30 @@
+﻿var productCategoryController = function () {
+    this.initialize = function () {
+        loadData();
+    }
+    function loadData() {
+        $.ajax({
+            url: '/admin/productCategory/getall',
+            dataType: 'json',
+            success: function (response) {
+                var data = [];
+                $.each(response, function (i, item) {
+                    data.push({
+                        id: item.Id,
+                        text: item.Name,
+                        parentId: item.ParentId,
+                        ord: item.Ord
+
+                    });
+                   
+                });
+                var treeArr = thiepvu.unflattern(data);
+                //var $tree = $('#treeProductCategogy');
+                $('#treeProductCategory').tree({
+                    data: treeArr,
+                    dnd:true
+                })
+            }
+        });
+    }
+}
